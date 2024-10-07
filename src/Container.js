b2ac97924eda5771d63c30a63d97d475c9e8ce32
aicha652 <aichaazlf652@gmail.com>
2024-10-07 23:43:51 +0200

    add Component

---
+++ src/Container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Container.css"
+import Star from './Star'
+
+export default function Container() {
+  return (
+    <div className="container">
+        <div><h2>How many stars would you give to our Online Code Editor?</h2></div>
+        <div><Star /></div>
+        <div><p>mmmmmmmmmmmmmmmm</p></div>
+    </div>
+  )
+}
